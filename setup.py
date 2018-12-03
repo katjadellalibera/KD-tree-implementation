9ea01a4a390c80815d52dc337d1ec5af819b0fc1
katjadellalibera <40602167+katjadellalibera@users.noreply.github.com>
2018-12-03 17:03:19 +0900

    setting up structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kd_tree",
+    version="0.0.1",
+    author="Katja Della Libera",
+    author_email="katja.dellalibera@minerva.kgi.edu",
+    description="An implementation of KD trees",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/katjadellalibera/KD-tree-implementation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
