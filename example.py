377d33afca89dcecd6da17c5ffb4027e12e8f04e
katjadellalibera <40602167+katjadellalibera@users.noreply.github.com>
2018-12-16 18:40:59 +0900

    started examples

--- example.py
+++ example.py
@@ -1,6 +1,11 @@
-import kd_tree
+import numpy as np
+import kd_tree as kd
+import json
 
-exampletree=build_tree(dict(enumerate(np.random.rand(1000,3).tolist())))
-print(distance(find_approx_nearest(exampletree,[0.2,0.7,0.9]).value,[0.2,0.7,0.9]),
-    find_approx_nearest(exampletree,[0.2,0.7,0.9]).value)
-print(find_exact_nearest(exampletree,[0.2,0.7,0.9]))
+exampletree=kd.build_tree(dict(enumerate(np.random.rand(1000,3).tolist())))
+print(kd.distance(kd.find_approx_nearest(exampletree,[0.2,0.7,0.9]).value,
+            [0.2,0.7,0.9]),
+    kd.find_approx_nearest(exampletree,[0.2,0.7,0.9]).value)
+print(kd.find_exact_nearest(exampletree,[0.2,0.7,0.9]))
+
+paintcolors=
